2dc3110dd44a7d57c9450dd1c239c78228597c7d
Ainhoa Lara <lara.153982@e.unavarra.es>
2025-09-26 17:03:04 +0200

    [#26] - Insertados 5 ejemplos a la tabla de medicamentos

---
+++ insertMedicamentos.sql
@@ -0,0 +1,6 @@
+INSERT INTO medicamentos (pactivo, foto, nombre_comercial, dosis, unidad) VALUES
+('IBUPROFENO', '', 'IBUPROFENO TARBIS FARMA', '600', 'mg'),
+('PARACETAMOL', '', 'FEBRECTAL LACTANTES', '150', 'mg'),
+('AMOXICILINA TRIHIDRATO', '', 'AMOXICILINA ARDINE', '500', 'mg'),
+('FENTANILO', '', 'ACTIQ', '200', 'µg'),
+('TRIHEXIFENIDIL HIDROCLORURO', '', 'ARTANE', '5', 'mg');
