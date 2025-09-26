e6e153f7238a41a5a0c7bcb563626b2f1b9d1a49
JavierMerinoPinedo <jmerinopinedo@gmail.com>
2025-09-26 12:14:25 +0200

    [#34] - Pantalla de mis medicamentos con boton de agragar diseñada

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -1,16 +1,56 @@
 import { Tabs } from "expo-router";
-import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function RootLayout() {
     return (
         <Tabs>
             <Tabs.Screen
                 name="index"
-                options={{ tabBarLabel: "Pantalla principal" }}
+                options={{
+                    tabBarLabel: "Pantalla principal",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+                        <MaterialIcons name="home" size={size} color={color} />
+                    ),
+                }}
             />
             <Tabs.Screen
-                name="home/index"
-                options={{ tabBarLabel: "Pantalla secundaria" }}
+                name="login/index"
+                options={{ tabBarLabel: "Login", headerShown: false,
+                    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+                        <MaterialIcons name="login" size={size} color={color} />
+                    ),
+                 }}
+            />
+            <Tabs.Screen
+                name="perfil/index"
+                options={{ 
+                    tabBarLabel: "Perfil", 
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+                        <MaterialIcons name="person" size={size} color={color} />
+                    ),
+                 }}
+            />
+            <Tabs.Screen
+                name="pastillas/index"
+                options={{
+                    tabBarLabel: "Pastillas",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+                        <MaterialIcons name="medication" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="calendario/index"
+                options={{
+                    tabBarLabel: "Calendario",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+                        <MaterialIcons name="calendar-today" size={size} color={color} />
+                    ),
+                }}
             />
         </Tabs>
     );
