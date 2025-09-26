bde68d2cc8072cb96fade57375bfd664a709d2f4
Rubén Zuabsti <ruben.zubasti@540deg.com>
2025-09-26 18:24:04 +0200

    [#34] - Cambiar nombre de la pagina

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -2,22 +2,32 @@ import { Tabs } from "expo-router";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function RootLayout() {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{ tabBarLabel: "Pantalla principal" }}
-      />
-      <Tabs.Screen
-        name="pastillas/Pastillas"
-        options={{
-          tabBarLabel: "Pastillas",
-          headerShown: false,
-          tabBarIcon: ({ color, size }: { color: string; size: number }) => (
-            <MaterialIcons name="medication" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{ tabBarLabel: "Pantalla principal" }}
+            />
+            <Tabs.Screen
+                name="tratamientos/MisTratamientos"
+                options={{
+                    tabBarLabel: "Mis Tratamientos",
+                    headerShown: false,
+                    tabBarIcon: ({
+                        color,
+                        size,
+                    }: {
+                        color: string;
+                        size: number;
+                    }) => (
+                        <MaterialIcons
+                            name="medication"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
