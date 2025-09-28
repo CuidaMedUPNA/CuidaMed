0f3d4dc15e9d15e3c98280ca80927291090e44da
AlexNagore <alexnagoreiri@gmail.com>
2025-09-28 15:14:30 +0200

    [#27] - Implementacion endpoint creacion tratamiento

--- back/src/handlers.ts
+++ back/src/handlers.ts
@@ -1,8 +1,12 @@
-import { RouteHandlers } from "@cuidamed-api/server";
+import { RouteHandlers, User } from "@cuidamed-api/server";
+import { NewTratamiento } from "./db/types";
+import * as tp from "./repository/treatmentRepository";
 
 export const handlers: RouteHandlers = {
   getUsers: async (request, reply) => {
-    const users = [{ id: "1", name: "Usuario1", email: "usuario1@gmail.com" }];
+    const users: User[] = [
+      { id: "1", name: "Usuario1", email: "usuario1@gmail.com" },
+    ];
     await reply.status(200).send(users);
   },
   healthCheck: async (request, reply) => {
@@ -16,5 +20,17 @@ export const handlers: RouteHandlers = {
     } as const;
 
     await reply.status(200).send(medicationTake);
+  },
+  createTreatment: async (request, reply) => {
+    const treatment = request.body;
+
+    console.log("Received treatment:", treatment);
+    const newTreatment: NewTratamiento = {
+      nombre: treatment.name,
+      id_usuario: Number(treatment.userId),
+    };
+    console.log("New treatment to insert:", newTreatment);
+    await tp.newTreatment(newTreatment);
+    await reply.status(200).send(treatment);
   },
 };
