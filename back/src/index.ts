3b302988fb47ea5deb25f31184b314cd7ad08f7a
Manex Tiraplegui <130599928+Manex142@users.noreply.github.com>
2025-09-29 22:41:40 +0200

    [] - Generar documentacion de la API (#47)
    Co-authored-by: Alejando Nagore Irigoyen <114134209+Alexnagore@users.noreply.github.com>

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify";
 import { handlers } from "./handlers";
 import openapiGlue from "fastify-openapi-glue";
 import fastifyCors from "@fastify/cors";
+import fastifyStatic from "@fastify/static";
 
 const fastify = Fastify({ logger: true });
 
@@ -12,6 +13,13 @@ const options = {
 };
 fastify.register(openapiGlue, options);
 fastify.register(fastifyCors, { origin: "*" });
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "../../docs"),
+});
+
+fastify.get("/documentation", async (request, reply) => {
+  return reply.type("text/html").sendFile("/api.html");
+});
 
 fastify.listen({ port: 3000 }, (err, address) => {
   if (err) {
