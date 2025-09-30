b429cfdc9822df6e772b2be8db9b7fe7e19de8d3
Alejando Nagore Irigoyen <114134209+Alexnagore@users.noreply.github.com>
2025-09-30 18:38:29 +0200

    [#27] - Implementar endpoint creacion tratamiento (#50)
    * [#27] - Implementacion endpoint creacion tratamiento
    
    * [#27] - Implementacion endpoint creacion tratamiento
    
    * [#27] - Modificada inportacion del metodo de insertar tratamiento
    
    * [#27] -  Eliminado gitignore del back

--- back/src/handlers.ts
+++ back/src/handlers.ts
@@ -1,8 +1,12 @@
-import { RouteHandlers } from "@cuidamed-api/server";
+import { RouteHandlers, User } from "@cuidamed-api/server";
+import { NewTratamiento } from "./db/types";
+import { insertTreatment } from "./repository/treatmentRepository";
 
 export const handlers: RouteHandlers = {
   getUsers: async (request, reply) => {
-    const users = [{ id: "1", name: "Usuario1", email: "usuario1@gmail.com" }];
+    const users: User[] = [
+      { id: "1", name: "Usuario1", email: "usuario1@gmail.com" },
+    ];
     await reply.status(200).send(users);
   },
   healthCheck: async (request, reply) => {
@@ -16,5 +20,16 @@ export const handlers: RouteHandlers = {
     } as const;
 
     await reply.status(200).send(medicationTake);
+  },
+  createTreatment: async (request, reply) => {
+    const treatment = request.body;
+
+    const newTreatment: NewTratamiento = {
+      nombre: treatment.name,
+      id_usuario: Number(treatment.userId),
+    };
+
+    await insertTreatment(newTreatment);
+    await reply.status(200).send(treatment);
   },
 };
--- back/src/repository/treatmentRepository.ts
+++ back/src/repository/treatmentRepository.ts
@@ -1,7 +1,7 @@
 import { db } from "../db/database";
 import { NewTratamiento } from "../db/types";
 
-export async function createTreatment(treatment: NewTratamiento) {
+export async function insertTreatment(treatment: NewTratamiento) {
   return await db
     .insertInto("tratamientos")
     .values(treatment)
