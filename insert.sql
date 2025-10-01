8bc098982dee5a2c7ccc7f13fcd193aca361646b
Ainhoa Lara <lara.153982@e.unavarra.es>
2025-10-01 16:17:09 +0200

    [#26] - Insertados ejemplos para medicine y user

--- insert.sql
+++ insert.sql
@@ -1,6 +1,9 @@
-INSERT INTO medicamentos (pactivo, foto, nombre_comercial, dosis, unidad) VALUES
+INSERT INTO medicine (pactive, picture, trade_name, dose, unit) VALUES
 ('IBUPROFENO', '', 'IBUPROFENO TARBIS FARMA', '600', 'mg'),
 ('PARACETAMOL', '', 'FEBRECTAL LACTANTES', '150', 'mg'),
 ('AMOXICILINA TRIHIDRATO', '', 'AMOXICILINA ARDINE', '500', 'mg'),
 ('FENTANILO', '', 'ACTIQ', '200', 'µg'),
 ('TRIHEXIFENIDIL HIDROCLORURO', '', 'ARTANE', '5', 'mg');
+INSERT INTO "user" ("name", email, "password", birthdate, profile_picture, gender) VALUES
+('Ainhoa', 'ainhoa@gmail.com', '1234', '2004-08-14', 'https://randomuser.me/api/portraits', 'Female');
+
