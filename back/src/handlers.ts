f0c7c440a940f635a26226c95588dd18f006dfc6
Alejando Nagore Irigoyen <114134209+Alexnagore@users.noreply.github.com>
2025-10-03 17:57:00 +0200

    [#53] - Implementar endpoint consulta tratamientos (#59)
    * [#53] - Modificado handler para incluir nuevo endopoint
    
    * [#53] - Implementado endpoint consulta tratamientos
    
    * [#53] - Implementado endpoit de consulta de tratamientos
    
    * [#53] - Corregidos errores con las fechas
    
    * [#53] - Eliminados mensaje en consola de pruebas

--- back/src/handlers.ts
+++ back/src/handlers.ts
@@ -1,35 +1,33 @@
-import { RouteHandlers, User } from "@cuidamed-api/server";
-import { NewTratamiento } from "./db/types";
-import { insertTreatment } from "./repository/treatmentRepository";
+import { RouteHandlers } from "@cuidamed-api/server";
+import { NewTreatment } from "./db/types";
+import {
+  insertTreatment,
+  getTreatmentsByUserId,
+} from "./repository/treatmentRepository";
 
 export const handlers: RouteHandlers = {
-  getUsers: async (request, reply) => {
-    const users: User[] = [
-      { id: "1", name: "Usuario1", email: "usuario1@gmail.com" },
-    ];
-    await reply.status(200).send(users);
-  },
   healthCheck: async (request, reply) => {
     await reply.status(200).send({ status: "ok" });
   },
-  getMedicationTake: async (request, reply) => {
-    const medicationTake = {
-      medicationId: "med1",
-      time: new Date().toISOString(),
-      status: "taken",
-    } as const;
 
-    await reply.status(200).send(medicationTake);
-  },
   createTreatment: async (request, reply) => {
     const treatment = request.body;
 
-    const newTreatment: NewTratamiento = {
-      nombre: treatment.name,
-      id_usuario: Number(treatment.userId),
+    const newTreatment: NewTreatment = {
+      name: treatment.name,
+      user_id: Number(treatment.userId),
+      start_date: new Date(treatment.startDate),
+      end_date: new Date(treatment.endDate),
     };
 
     await insertTreatment(newTreatment);
     await reply.status(200).send(treatment);
+  },
+  getTreatments: async (request, reply) => {
+    const userId = request.query.userId;
+
+    const treatments = await getTreatmentsByUserId(userId);
+
+    await reply.status(200).send(treatments);
   },
 };
--- back/src/repository/treatmentRepository.ts
+++ back/src/repository/treatmentRepository.ts
@@ -1,10 +1,33 @@
 import { db } from "../db/database";
-import { NewTratamiento } from "../db/types";
+import { NewTreatment } from "../db/types";
+import { Treatment } from "@cuidamed-api/server";
 
-export async function insertTreatment(treatment: NewTratamiento) {
+export async function insertTreatment(treatment: NewTreatment) {
   return await db
-    .insertInto("tratamientos")
+    .insertInto("treatment")
     .values(treatment)
     .returningAll()
     .executeTakeFirstOrThrow();
+}
+
+export async function getTreatmentsByUserId(userId: number) {
+  const today = new Date();
+
+  const treatments = await db
+    .selectFrom("treatment")
+    .selectAll()
+    .where("user_id", "=", userId)
+    .where("end_date", ">", today)
+    .where("start_date", "<=", today)
+    .execute();
+
+  const treatmentsWithDates: Treatment[] = treatments.map((treatment) => ({
+    name: treatment.name,
+    userId: treatment.user_id,
+    id: treatment.id,
+    startDate: treatment.start_date?.toDateString() || "",
+    endDate: treatment.end_date?.toDateString() || "",
+  }));
+
+  return treatmentsWithDates;
 }
