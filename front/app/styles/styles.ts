bac97e32efef02bfbb11f41c09b40a2aea4d410b
Rubén Zuabsti <ruben.zubasti@540deg.com>
2025-10-10 18:34:11 +0200

    [#70] - Realizar llamada a back real y mostrar listado de tratamientos reales

---
+++ front/app/styles/styles.ts
@@ -0,0 +1,11 @@
+export const titleStyle = {
+  fontSize: 30,
+  marginBottom: 20,
+  color: "#e03535ff",
+};
+
+export const subtitleStyle = {
+  fontSize: 20,
+  marginBottom: 10,
+  color: "#6b6b6bff",
+};
