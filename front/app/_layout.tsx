aaaaf45b93f537f26a1af1ea27be9250dfebeff0
Javier <117859441+JavierMerinoPinedo@users.noreply.github.com>
2025-10-17 00:23:43 +0200

    [#54] - Añadida fecha inicio y fecha fin a nuevo tratamiento. Implementado también llamada a la API (#78)
    Para grabar el vieo

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -2,35 +2,32 @@ import { Tabs } from "expo-router";
 import { MaterialIcons } from "@expo/vector-icons";
 import "@/i18n";
 import { useTranslation } from "react-i18next";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-    const { t } = useTranslation();
-    return (
-        <Tabs>
-            <Tabs.Screen
-                name="index"
-                options={{ tabBarLabel: `${t('landingPageTitle')}` }}
-            />
-            <Tabs.Screen
-                name="tratamientos/MisTratamientos"
-                options={{
-                    tabBarLabel: `${t('treatmentsTitle')}`,
-                    headerShown: false,
-                    tabBarIcon: ({
-                        color,
-                        size,
-                    }: {
-                        color: string;
-                        size: number;
-                    }) => (
-                        <MaterialIcons
-                            name="medication"
-                            size={size}
-                            color={color}
-                        />
-                    ),
-                }}
-            />
-        </Tabs>
-    );
+  const { t } = useTranslation();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{ tabBarLabel: `${t("landingPageTitle")}` }}
+        />
+        <Tabs.Screen
+          name="tratamientos/MisTratamientos"
+          options={{
+            tabBarLabel: `${t("treatments.treatmentsTitle")}`,
+            headerShown: false,
+            tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+              <MaterialIcons name="medication" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </QueryClientProvider>
+  );
 }
