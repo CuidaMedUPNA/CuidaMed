4646b0b264c0eec7f7741353bf4530ab83f79622
AlexNagore <alexnagoreiri@gmail.com>
2025-10-20 21:10:50 +0200

    [] - Elimitada tabla intake_treatment, modificada tabla intake para añadir treatment_id en vez de user_id

--- createTables.sql
+++ createTables.sql
@@ -36,14 +36,5 @@ CREATE TABLE intake (
     dose_intake VARCHAR(100),
     hour TIME,
     FOREIGN KEY (medicine_id) REFERENCES medicine(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
-);
-
-CREATE TABLE intake_treatment (
-    intake_id INT NOT NULL,
-    treatment_id INT NOT NULL,
-    PRIMARY KEY (intake_id, treatment_id),
-    FOREIGN KEY (intake_id) REFERENCES intake(id) ON DELETE CASCADE,
     FOREIGN KEY (treatment_id) REFERENCES treatment(id) ON DELETE CASCADE
 );
-
