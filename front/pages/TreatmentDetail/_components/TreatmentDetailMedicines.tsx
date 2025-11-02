8232709e20ab5f09bd93b508b4e3352b4891de09
Ainhoa Lara <lara.153982@e.unavarra.es>
2025-11-02 13:48:41 +0100

    [#132] - Agregado botón de añadir medicamento

--- front/pages/TreatmentDetail/_components/TreatmentDetailMedicines.tsx
+++ front/pages/TreatmentDetail/_components/TreatmentDetailMedicines.tsx
@@ -1,4 +1,10 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import {
   AssociatedMedicine,
   AssociatedMedicineProps,
@@ -22,7 +28,18 @@ export const TreatmentDetailMedicines = ({
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Medicamentos asociados</Text>
+      <View style={styles.headerRow}>
+        <Text style={styles.title}>Medicamentos asociados</Text>
+        <TouchableOpacity
+          style={styles.addMedicineButton}
+          onPress={() => {
+            // Lógica para agregar un nuevo medicamento
+          }}
+          accessibilityLabel="Agregar medicamento"
+        >
+          <Text style={styles.addMedicineButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
       <FlatList
         data={medicines}
         renderItem={renderMedicine}
@@ -53,5 +70,28 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     paddingBottom: 16,
+  },
+  headerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 16,
+    marginBottom: 16,
+  },
+  addMedicineButton: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    backgroundColor: "#f23728",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addMedicineButtonText: {
+    fontSize: 24,
+    color: "#ffffffff",
+    fontWeight: "600",
+    textAlignVertical: "center",
+    textAlign: "center",
+    transform: [{ translateY: -2 }],
   },
 });
