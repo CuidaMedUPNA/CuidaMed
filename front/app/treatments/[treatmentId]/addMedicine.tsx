e66811b13cb2e581b90c5d3a8817a4777451032d
Ainhoa Lara <lara.153982@e.unavarra.es>
2025-11-07 17:29:49 +0100

    [#133] - Modificar ruta de añadir medicamento

--- front/app/treatments/[treatmentId]/addMedicine.tsx
+++ front/app/treatments/[treatmentId]/addMedicine.tsx
@@ -1,7 +1,12 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AddMedicinePage } from "@/pages/AddMedicine/AddMedicinePage";
+import { useLocalSearchParams } from "expo-router";
 
 export default function AddMedicine() {
+  const { treatmentId: treatmentIdStr } = useLocalSearchParams();
+  const treatmentId = treatmentIdStr
+    ? parseInt(treatmentIdStr as string, 10)
+    : undefined;
   return (
     <SafeAreaProvider>
       <AddMedicinePage />
