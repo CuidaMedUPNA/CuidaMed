c0e347b7a9dd2c9e18b5c48f5f8fa15a4dab6399
Javier <117859441+JavierMerinoPinedo@users.noreply.github.com>
2025-11-11 18:40:06 +0100

    [#152] - Implementar eliminar una toma de un tratamiento (#172)
    * [#152] - Creado handle de eliminar toma
    
    * [#152[ - Añadido modal de pregunta

--- front/pages/TreatmentDetail/_components/TreatmentDetailMedicines.tsx
+++ front/pages/TreatmentDetail/_components/TreatmentDetailMedicines.tsx
@@ -4,29 +4,75 @@ import {
   FlatList,
   StyleSheet,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { AssociatedMedicine } from "./AssociatedMedicine";
-import { DosingSchedule } from "@cuidamed-api/client";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useRouter } from "expo-router";
+import {
+  DosingSchedule,
+  deleteIntakeMutation,
+  getIntakesByTreatmentOptions,
+} from "@cuidamed-api/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { t } from "i18next";
+
 export interface TreatmentDetailMedicinesProps {
   medicines: DosingSchedule[];
+  treatmentId: number;
 }
 
 export const TreatmentDetailMedicines = ({
   medicines,
+  treatmentId,
 }: TreatmentDetailMedicinesProps) => {
   const router = useRouter();
+  const queryClient = useQueryClient();
   const renderMedicine = ({ item }: { item: DosingSchedule }) => (
     <AssociatedMedicine
       name={item.medicineName}
       dose={item.doseAmount}
       unit={item.doseUnit}
       schedule={item.dosingTimes}
+      onPress={() => handleDeleteIntake(item.id, item.treatmentId)}
     />
   );
 
-  const { treatmentId } = useLocalSearchParams();
+  const mutation = useMutation({
+    ...deleteIntakeMutation(),
+    onSuccess: () => {
+      console.log("✅ Tratamiento eliminado con éxito.");
+      queryClient.invalidateQueries({
+        queryKey: getIntakesByTreatmentOptions({
+          path: {
+            treatmentId,
+          },
+        }).queryKey,
+      });
+    },
+    onError: (error) => {
+      console.error("❌ Error al eliminar tratamiento:", error);
+      Alert.alert(t("error"), t("treatments.create.errorAlert"));
+    },
+  });
 
+  const handleDeleteIntake = (intakeId: number, treatmentId: number) => {
+    Alert.alert(t("warning"), t("treatments.delete.confirmTitle"), [
+      { text: t("cancel"), style: "cancel" },
+      {
+        text: t("delete"),
+        style: "destructive",
+        onPress: () => {
+          console.log("Eliminar medicamento asociado con ID:", intakeId);
+          mutation.mutate({
+            path: {
+              treatmentId,
+              intakeId: intakeId,
+            },
+          });
+        },
+      },
+    ]);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.headerRow}>
@@ -36,7 +82,7 @@ export const TreatmentDetailMedicines = ({
           onPress={() => {
             router.push({
               pathname: "/treatments/[treatmentId]/addMedicine",
-              params: { treatmentId: treatmentId as string },
+              params: { treatmentId: treatmentId as number },
             });
           }}
           accessibilityLabel="Agregar medicamento"
